a11a2b1bd9e443b3d83455ee10d2fe47dd466fda
MayconRMS <maycon_ricardo_ms@live.com>
2021-09-09 18:23:50 -0300

    init

---
+++ my-meetings/src/main/java/br/com/mrms/meetings/MyMeetingsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrms.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMeetingsApplication.class, args);
+	}
+
+}
