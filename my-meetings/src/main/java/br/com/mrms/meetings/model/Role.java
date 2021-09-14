037a92ec26e75fd9e05f18df7c2dc5c581fc67b9
MayconRMS <maycon_ricardo_ms@live.com>
2021-09-14 19:38:33 -0300

    add relacionamentos

--- my-meetings/src/main/java/br/com/mrms/meetings/model/Role.java
+++ my-meetings/src/main/java/br/com/mrms/meetings/model/Role.java
@@ -1,7 +1,5 @@
 package br.com.mrms.meetings.model;
 
-import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,8 +7,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -19,29 +15,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "meetings")
-public class Meeting {
+@Table(name = "roles")
+public class Role {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
-	@Column(name = "description", nullable = false, length = 150)
-	private String description;
-
 	@Enumerated(EnumType.STRING)
-	private MeetingStatus status;
-
-	private LocalDate dateMeeting;
-
-	private boolean viseble;
-
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private MeetingCategory category;
-
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private User user;
+	@Column(length = 20)
+	private ERole name;
 
 }
--- my-meetings/src/main/java/br/com/mrms/meetings/model/User.java
+++ my-meetings/src/main/java/br/com/mrms/meetings/model/User.java
@@ -1,9 +1,18 @@
 package br.com.mrms.meetings.model;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -18,10 +27,19 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	
+
 	private String name;
 
 	private String password;
 
+	// Facilitar a exibição das reunioes por usuario
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Meeting> meetings;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "users_roles", 
+	joinColumns = @JoinColumn(name = "usuario_id"), 
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 }
- 
