152b9333e3e13cd8938e2dcca36238aef958ce57
MayconRMS <maycon_ricardo_ms@live.com>
2021-09-14 19:58:10 -0300

    teste repository initDatabase

--- my-meetings/src/main/java/br/com/mrms/meetings/repository/MeetingCategoryRepository.java
+++ my-meetings/src/main/java/br/com/mrms/meetings/repository/MeetingCategoryRepository.java
@@ -3,8 +3,10 @@ package br.com.mrms.meetings.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.mrms.meetings.model.MeetingCategory;
+
 
 @Repository
-public interface MeetingCategoryRepository extends JpaRepository<MeetingCategoryRepository, Integer> {
+public interface MeetingCategoryRepository extends JpaRepository<MeetingCategory, Integer> {
 
 }
