6eeee97f7321e024508e3f1b620704e519fef12e
MayconRMS <maycon_ricardo_ms@live.com>
2021-09-27 21:20:43 -0300

    Add conf de Erros

---
+++ my-meetings/src/main/java/br/com/mrms/meetings/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.mrms.meetings.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<String> erros = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(x -> x.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.badRequest().body(erros);
+	}
+
+}
